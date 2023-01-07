56ad8ee18e1a7afdec16ba014d65f3b7efe9675f
Rodrigo Seguel <FeaRushb@gmail.com>
2023-01-07 13:53:05 -0300

    fixed socket.io connection

--- client/static/js/chat.js
+++ client/static/js/chat.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000', {
+const socket = io(`https://chaterty.up.railway.app/`, {
     auth: {
         token: localStorage.getItem('jwt').split(' ')[1]
       } 
